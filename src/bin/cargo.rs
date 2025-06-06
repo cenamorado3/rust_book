a15649d54a43d73dea23a3897dabc758d74a5414
Christian Enamorado <cenamorado3@gmail.com>
2025-06-05 22:18:04 -0500

    chapters 1, 2

---
+++ src/bin/cargo.rs
@@ -0,0 +1,29 @@
+//cargo == nuget/pip/npm
+//cargo --version
+
+
+//to create a new project with CLI
+//cargo new "name"
+
+
+//TOML (Tom’s Obvious, Minimal Language)
+//crates == packages
+
+
+//build cargo with
+//cargo build
+
+
+//cargo or cargo -h command for help
+
+//target dir is created with cargo build
+//in the target folder, is a debug folder with a  "cargo_name" executable
+
+//to execute
+//cargo run 
+
+//cargo check
+//to verify it compiles without building
+
+//cargo build --release
+//like cargo build, creates a folder in target, named release
---
+++ src/bin/hello_world.rs
@@ -0,0 +1,8 @@
+fn main(){
+    println!("hello again");
+}
+
+//to compile and run
+//rustc helloworld.rs 
+//this will create binaries/executables which can be executed
+//following the naming convention, "hello_world.rs" results in the "hello_world" executable which can be executed via "hello_world"
